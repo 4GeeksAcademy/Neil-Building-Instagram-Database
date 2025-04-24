d43372bb4e88858c4310985e51fada8a598545b4
Neil-Player456 <Nmrubix11@gmail.com>
2025-04-24 01:12:46 +0000

    Fin

--- src/models.py
+++ src/models.py
@@ -17,3 +17,54 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    photo: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    video: Mapped[str] = mapped_column(String(120), nullable=False)
+    caption: Mapped[str] = mapped_column(String(120), nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "photo": self.photo,
+            "video": self.video,
+            "caption": self.caption,
+        }
+
+
+
+class Shop(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    photo: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    video: Mapped[str] = mapped_column(String(120), nullable=False)
+    description: Mapped[str] = mapped_column(String(120), nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "photo": self.photo,
+            "video": self.video,
+            "description": self.description,
+        }
+
+
+
+class DirectMessage(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(60), unique=True, nullable=False)
+    message: Mapped[str] = mapped_column(String(120), nullable=False)
+    friend: Mapped[str] = mapped_column(String(60), nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "message": self.message,
+            "friend": self.friend,
+        }
